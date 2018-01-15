9b6602888e4aeed496996d50f601418b2e48d345
Shubham Kumar <developershubh13@gmail.com>
2018-01-15 20:25:20 +0530

    shubham-profile layout

--- app/src/main/java/ojass/in/ojass_18/MainActivity.java
+++ app/src/main/java/ojass/in/ojass_18/MainActivity.java
@@ -1,13 +1,44 @@
 package ojass.in.ojass_18;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppBarLayout appbar;
+    CollapsingToolbarLayout ctool;
+    ImageView iv1;
+    Toolbar toolbar;
+    CircleImageView itemImage;
+
+
+    boolean ExpandedActionBar =true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar=(Toolbar)findViewById(R.id.tool);
+        ctool=(CollapsingToolbarLayout)findViewById(R.id.ctool);
+        iv1=(ImageView)findViewById(R.id.iv1);
+        appbar=(AppBarLayout)findViewById(R.id.appbar);
+        itemImage=(CircleImageView)findViewById(R.id.itemImage);
+
+        setSupportActionBar(toolbar);
+
+
     }
 }
+
+
+
+
+
