51724262433ccf79ea2f2d084747d6484ede5d65
anirudh1107 <anirudhdeep.11@gmail.com>
2018-01-22 01:09:14 +0530

    initial

---
+++ app/src/main/java/ojass/in/ojass_18/Activities/FacebookAccountKitActivity.java
@@ -0,0 +1,79 @@
+package ojass.in.ojass_18.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.accountkit.AccountKitLoginResult;
+import ojass.in.ojass_18.R;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+
+public class FacebookAccountKitActivity extends BaseActivity implements View.OnClickListener {
+    public static int APP_REQUEST_CODE = 99;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_account_kit);
+
+        AccessToken accessToken = AccountKit.getCurrentAccessToken();
+        Button button=findViewById(R.id.activity_facebook_account_kit_verify);
+        button.setOnClickListener(this);
+        if (accessToken != null) {
+            //Handle Returning User
+        } else {
+            phoneLogin(button);
+
+        }
+    }
+
+    public void phoneLogin(final View view) {
+        final Intent intent = new Intent(this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                        LoginType.PHONE,
+                        AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
+        // ... perform additional configuration ...
+        intent.putExtra(
+                AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                configurationBuilder.build());
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == APP_REQUEST_CODE) { // confirm that this response matches your request
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            String toastMessage;
+            if (loginResult.getError() != null) {
+                toastMessage = loginResult.getError().getErrorType().getMessage();
+
+            } else if (loginResult.wasCancelled()) {
+                toastMessage = "Login Cancelled";
+            } else {
+                if (loginResult.getAccessToken() != null) {
+                    toastMessage = "Success:" + loginResult.getAccessToken().getAccountId();
+                } else {
+                    toastMessage = String.format(
+                            "Success:%s...",
+                            loginResult.getAuthorizationCode().substring(0, 10));
+                }
+            }
+
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
