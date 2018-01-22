f19fdbfcd4354206da98ba8ccc421c96cebccefe
anirudh1107 <anirudhdeep.11@gmail.com>
2018-01-23 01:01:40 +0530

    auto popup gurugyan

--- app/src/main/java/ojass/in/ojass_18/Activities/GurugyanActivity.java
+++ app/src/main/java/ojass/in/ojass_18/Activities/GurugyanActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -62,7 +63,7 @@ public class GurugyanActivity extends BaseActivity {
         //setting page transform animation
         GurugyanPager.setPageTransformer(false,new IntroPageTransform());
         guruName.setText(guruList.get(0).getName());
-
+        expandView();
         //expanding and contracting the bottom card
         OnTap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,7 +90,7 @@ public class GurugyanActivity extends BaseActivity {
             @Override
             public void onPageSelected(final int position) {
 
-
+                expandableView.setVisibility(View.GONE);
 
                 //changing the text of each slide with animation
                 GurugyanModel current=guruList.get(position);
@@ -106,9 +107,11 @@ public class GurugyanActivity extends BaseActivity {
                     guruName.setAnimation(slideInLeft);
 
                 }
+
                 //setting the previous position
                 positionpre=position;
 
+                expandView();
             }
 
             @Override
@@ -120,6 +123,18 @@ public class GurugyanActivity extends BaseActivity {
 
 
 
+
+    }
+
+    public void expandView()
+    {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                expandableView.setVisibility(View.VISIBLE);
+            }
+        }, 2*1000);
 
     }
 }
